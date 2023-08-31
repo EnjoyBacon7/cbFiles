c978e2da146da9500e3e5ffa2d2864703fed899c
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-08-31 13:47:32 -0400

    icloud backup

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import CbShare from './components/CbShare';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col >
+          <CbShare />
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ web/src/components/CbShare.js
@@ -0,0 +1,15 @@
+import Card from 'react-bootstrap/Card';
+
+export function CbShare() {
+    return(
+        <div>
+            <Card>
+                <Card.Body>
+                    test
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default CbShare;
