124e6cd155683eee9d19cbdc170cf1c9e54869fd
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-01 17:23:23 -0400

    Added Comments and Browser router

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,40 @@
-import CbShare from './components/CbShare';
-
+// System imports
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-import './App.css';
+// Local imports
+import CbRoot from './routes/CbRoot';
+import ErrorPage from './routes/error-page';
+import CbShare from './routes/CbShare';
 
+// Router Initialization
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CbRoot />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/share/:shareId",
+    element: <CbShare />,
+  }
+]);
+
+// Main App Component
 function App() {
   return (
     <Container>
       <Row>
         <Col></Col>
         <Col lg='8'>
-          <CbShare />
+
+          <RouterProvider router={router} />
+
         </Col>
         <Col></Col>
       </Row>
