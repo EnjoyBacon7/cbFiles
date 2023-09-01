5f1fe8c06c73356c525e074952330eeb537bc39e
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-01 17:23:57 -0400

    Added File Component

--- web/src/components/CbFile.js
+++ web/src/components/CbFile.js
@@ -1,14 +1,43 @@
+// System imports
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-export function CbFile(props) {
-    return(
+// File card component
+export function CbFile(props) {
+    const [isShown, setIsShown] = useState(false);
+
+    return (
         <div className="mt-3">
-            <Card>
-                <Card.Body className="text-center pt-3 pb-3 ps-0 pe-0">
-                    <Image src="./fileIcons/png.png" width={150} />
+            <Card
+                className="text-center pt-3 pb-3 ps-0 pe-0"
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}>
+                <Card.Body>
+                    <Image src="../fileIcons/png.png" width={150} />
                     <Card.Title className="m-0">{props.fileName}</Card.Title>
+
                 </Card.Body>
+                {isShown && (
+                    <div className='position-absolute top-0 end-0 m-2'>
+                        <ButtonGroup>
+                            <Button variant='outline-secondary'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                                </svg>
+                            </Button>
+                            <Button variant='outline-danger'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                                </svg>
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                )}
             </Card>
         </div>
     );
