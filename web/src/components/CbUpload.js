5b0565da0c4fb4e696aca29c6502ffd4112e1de8
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-01 19:05:42 -0400

    Simplified Upload Button

--- web/src/components/CbUpload.js
+++ web/src/components/CbUpload.js
@@ -1,21 +1,13 @@
 // System imports
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 // Upload component
 export function CbUpload() {
     return (
-        <div>
-            <Card>
-                <Card.Body className='text-center'>
-                    <Row>
-                        <Col xs='8' className='my-auto'>Drag and Drop your files here or...</Col>
-                        <Col xs='4' className='my-auto'><Button variant='primary' className=''>Upload</Button></Col>
-                    </Row>
-                </Card.Body>
-            </Card>
+        <div className='d-flex flex-column'>
+            <Button variant='outline-secondary my-aut p-5'>
+                Add Files Here
+            </Button>
         </div>
     );
 }
