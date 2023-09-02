256a259bed9f6ef72cb1ee83f93d1d72dc4ff7bb
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-02 17:15:21 -0400

    Added file fetching to create react elements

--- web/src/routes/CbShare.js
+++ web/src/routes/CbShare.js
@@ -1,4 +1,5 @@
 // System imports
+import React, { useEffect, useState } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 
@@ -8,28 +9,38 @@ import CbFile from '../components/CbFile';
 
 // Share instance component
 export function CbShare() {
+
+    const [components, setComponents] = useState([]);
+    const shareId = window.location.pathname.split('/')[2];
+
+    useEffect(() => {
+        fetch(`/api?shareId=${shareId}`).then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        }).then(data => {
+            const files = data.files;
+            console.log(files, files.length);
+            for (let i = 0; i < files.length; i++) {
+                const newComponents = files.map((item) => (
+                    <Col className="mt-3" lg={4} key={item.id}>
+                        <CbFile fileName={item} />
+                    </Col>
+                    ));
+                setComponents(newComponents);
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+    }, [shareId]);
+
     return (
         <div>
             <CbUpload />
             <div>
                 <Row className='justify-content-center'>
-                    <Col lg={4}><CbFile fileName='test'/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile/></Col>
-                    <Col lg={4}><CbFile /></Col>
-                    <Col lg={4}><CbFile /></Col>
+                    {components}
                 </Row>
             </div>
         </div>
