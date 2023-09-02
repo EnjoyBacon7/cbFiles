ac84a064fb636bba8040dbd432ccaa4a2253cb5c
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-02 17:14:55 -0400

    Added Search field and redirect

--- web/src/routes/CbRoot.js
+++ web/src/routes/CbRoot.js
@@ -1,11 +1,32 @@
 // Local imports
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import CbUpload from "../components/CbUpload";
+import Form from "react-bootstrap/Form";
 
 // Root Component
 export function CbRoot() {
+
+    const [shareId, setShareId] = useState("");
+    const navigate = useNavigate();
+
+    function handleChange(e) {
+        setShareId(e.target.value);
+    }
+
+    function handleRedirect() {
+        navigate(`/share/${shareId}`);
+    }
+
+
     return (
         <div>
             <CbUpload />
+            <Form className="mt-3" onChange={handleChange} onSubmit={handleRedirect}>
+                <Form.Group>
+                    <Form.Control type="text" placeholder="Enter Share ID to Retrieve an Existing Share" />
+                </Form.Group>
+            </Form>
         </div>
     );
 }
