5acdc7381ffee36ecca1f80b2a31e064e73dac94
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-02 23:55:44 -0400

    Added Download HTTP handler

--- server/main.go
+++ server/main.go
@@ -89,6 +89,7 @@ func main() {
 	http.HandleFunc("/api/search", share.HandleSearch)
 	http.HandleFunc("/api/upload", share.HandleUpload)
 	http.HandleFunc("/api/delete", share.HandleDelete)
+	http.HandleFunc("/api/download", share.HandleDownload)
 	http.Handle("/", intercept404(fileServer, serveIndex))
 
 	http.ListenAndServe(":8080", nil)
