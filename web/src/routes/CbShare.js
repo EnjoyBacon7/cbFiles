e909166eba3cade5f4a5be38abdec208646ff5f2
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-02 22:24:33 -0400

    Implemented reload on upload

--- web/src/routes/CbShare.js
+++ web/src/routes/CbShare.js
@@ -13,15 +13,15 @@ export function CbShare() {
     const [components, setComponents] = useState([]);
     const shareId = window.location.pathname.split('/')[2];
 
-    useEffect(() => {
-        fetch(`/api?shareId=${shareId}`).then(response => {
+    function loadFiles() {
+        console.log("loading files")
+        fetch(`/api/search?shareId=${shareId}`).then(response => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
             return response.json();
         }).then(data => {
             const files = data.files;
-            console.log(files, files.length);
             for (let i = 0; i < files.length; i++) {
                 const newComponents = files.map((item) => (
                     <Col className="mt-3" lg={4} key={item.id}>
@@ -33,11 +33,15 @@ export function CbShare() {
         }).catch(error => {
             console.log(error);
         });
-    }, [shareId]);
+    }
+
+    useEffect(() => {
+        loadFiles();
+    }, []);
 
     return (
         <div>
-            <CbUpload />
+            <CbUpload loadFiles={loadFiles}/>
             <div>
                 <Row className='justify-content-center'>
                     {components}
