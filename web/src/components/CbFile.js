c24be1c1b8719eb1c23146790982e7e6be6e1d9a
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-02 23:56:18 -0400

    Implemented Front-end download

--- web/src/components/CbFile.js
+++ web/src/components/CbFile.js
@@ -11,6 +11,8 @@ import isSupported from '../supportedFileTypes';
 // File card component
 export function CbFile(props) {
 
+    const downloadElement = document.createElement('a');
+
     const shareId = window.location.pathname.split('/')[2];
     const [isShown, setIsShown] = useState(false);
     var fileType;
@@ -35,8 +37,30 @@ export function CbFile(props) {
         fetch(`/api/delete?shareId=${shareId}&fileName=${fileName}`).then(response => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
-            }
+            }  
             props.loadFiles();
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    function handleDownload() {
+        const fileName = props.fileName;
+        fetch(`/api/download?shareId=${shareId}&fileName=${fileName}`).then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.blob();
+        }).then(blob => {
+
+            const blobUrl = window.URL.createObjectURL(blob);
+
+            downloadElement.href = blobUrl;
+            downloadElement.download = fileName;
+            downloadElement.click();
+
+            window.URL.revokeObjectURL(blobUrl);
+            
         }).catch(error => {
             console.log(error);
         });
@@ -56,7 +80,7 @@ export function CbFile(props) {
                 {isShown && (
                     <div className='position-absolute top-0 end-0 m-2'>
                         <ButtonGroup>
-                            <Button variant='outline-secondary'>
+                            <Button variant='outline-secondary' onClick={handleDownload}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
