9fd9e8cbe90d7f29fbc9334e35797639f563b401
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-03 10:41:01 -0400

    Added Share file search frontend (and view setter)

---
+++ web/src/components/CbShareNav.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+export function CbShareNav() {
+    const [displayMode, setDisplayMode] = useState("list");
+
+    const handleModeChange = (value) => {
+        setDisplayMode(value);
+    };
+
+    return (
+        <div>
+            <InputGroup className="mt-3">
+                <FormControl
+                    placeholder='Search Here'
+                    aria-label="file search field"
+                />
+                <ToggleButton
+                    id="list-btn"
+                    type="checkbox"
+                    variant="outline-secondary"
+                    value="list"
+                    checked={displayMode === 'list'}
+                    onChange={() => handleModeChange('list')}
+                    className='d-flex justify-content-center align-items-center col-1'
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-ul" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                    </svg>
+                </ToggleButton>
+                <ToggleButton
+                    id="gallery-btn"
+                    type="checkbox"
+                    variant="outline-secondary"
+                    value="gallery"
+                    checked={displayMode === 'gallery'}
+                    onChange={() => handleModeChange('gallery')}
+                    className='d-flex justify-content-center align-items-center col-1'
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-grid-fill" viewBox="0 0 16 16">
+                        <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
+                    </svg>
+                </ToggleButton>
+            </InputGroup>
+        </div>
+    );
+}
+
+export default CbShareNav;
