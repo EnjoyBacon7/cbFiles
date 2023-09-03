008bde70a657a8fff93f79a3f048104c53bfe896
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-02 22:37:41 -0400

    Fetch to delete in component

--- web/src/components/CbFile.js
+++ web/src/components/CbFile.js
@@ -10,6 +10,8 @@ import isSupported from '../supportedFileTypes';
 
 // File card component
 export function CbFile(props) {
+
+    const shareId = window.location.pathname.split('/')[2];
     const [isShown, setIsShown] = useState(false);
     var fileType;
     
@@ -26,6 +28,18 @@ export function CbFile(props) {
         } else {
             return '../fileIcons/file.png';
         }
+    }
+
+    function handleDelete() {
+        const fileName = props.fileName;
+        fetch(`/api/delete?shareId=${shareId}&fileName=${fileName}`).then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            props.loadFiles();
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
     return (
@@ -48,7 +62,7 @@ export function CbFile(props) {
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                                 </svg>
                             </Button>
-                            <Button variant='outline-danger'>
+                            <Button variant='outline-danger' onClick={handleDelete}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
