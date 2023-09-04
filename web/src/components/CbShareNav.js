f1e479cd374f82cf1b6b19c499a88f447fb99fd6
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-04 09:15:02 -0400

    Removed extra grid attachements

--- web/src/components/CbShareNav.js
+++ web/src/components/CbShareNav.js
@@ -20,7 +20,7 @@ export function CbShareNav({ changeViewMode, viewMode }) {
                     value="list"
                     checked={viewMode === 'list'}
                     onChange={() => changeViewMode('list')}
-                    className='d-flex justify-content-center align-items-center col-1'
+                    className='d-flex justify-content-center align-items-center'
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-ul" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
@@ -33,7 +33,7 @@ export function CbShareNav({ changeViewMode, viewMode }) {
                     value="gallery"
                     checked={viewMode === 'gallery'}
                     onChange={() => changeViewMode('gallery')}
-                    className='d-flex justify-content-center align-items-center col-1'
+                    className='d-flex justify-content-center align-items-center'
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-grid-fill" viewBox="0 0 16 16">
                         <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
