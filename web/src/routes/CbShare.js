d9d0a934d647b6c2c9e7562ab4561b57fa5c1060
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-03 21:41:34 -0400

    Moved file loading logic to CbFiles

--- web/src/routes/CbShare.js
+++ web/src/routes/CbShare.js
@@ -1,57 +1,32 @@
 // System imports
-import React, { useEffect, useState } from 'react';
-import Col from 'react-bootstrap/esm/Col';
-import Row from 'react-bootstrap/esm/Row';
+import React, { useState } from 'react';
 
 // Local imports
 import CbUpload from '../components/CbUpload';
-import CbFile from '../components/CbFile';
+import CbFiles from '../components/CbFiles';
 import CbHeader from '../components/CbHeader';
 import CbShareNav from '../components/CbShareNav';
 
 // Share instance component
 export function CbShare() {
 
-    const [components, setComponents] = useState([]);
-    const shareId = window.location.pathname.split('/')[2];
+    const [filesReloaded, setFilesReloaded] = useState(false);
+    const [viewMode, setViewMode] = useState("list");
 
-    function loadFiles() {
-        setComponents([]);
-        console.log("loading files")
-        fetch(`/api/search?shareId=${shareId}`).then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        }).then(data => {
-            const files = data.files;
-            for (let i = 0; i < files.length; i++) {
-                const newComponents = files.map((item) => (
-                    <Col className="mt-3" lg={4} key={item.id}>
-                        <CbFile fileName={item} loadFiles={loadFiles} />
-                    </Col>
-                    ));
-                setComponents(newComponents);
-            }
-        }).catch(error => {
-            console.log(error);
-        });
+    function reloadCbFilescomponent() {
+        setFilesReloaded(!filesReloaded);
     }
 
-    useEffect(() => {
-        loadFiles();
-    }, []);
+    function changeViewMode(mode) {
+        setViewMode(mode);
+    }
 
     return (
         <div>
             <CbHeader />
-            <CbUpload loadFiles={loadFiles}/>
-            <CbShareNav />
-            <div>
-                <Row className='justify-content-center'>
-                    {components}
-                </Row>
-            </div>
+            <CbUpload reloadCbFilescomponent={reloadCbFilescomponent} />
+            <CbShareNav changeViewMode={changeViewMode} viewMode={viewMode} />
+            <CbFiles key={filesReloaded} viewMode={viewMode} />
         </div>
     );
 }
