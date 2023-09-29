2c2a0d120645f9e634033d2b8604c6697db162ee
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-09-29 15:47:58 -0400

    Updated comments and order

--- web/src/routes/CbRoot.js
+++ web/src/routes/CbRoot.js
@@ -1,28 +1,31 @@
-// Local imports
+// System imports
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import CbUpload from "../components/CbUpload";
 import Form from "react-bootstrap/Form";
 
 // Local imports
+import CbUpload from "../components/CbUpload";
 import CbHeader from "../components/CbHeader";
 
 // Root Component
 export function CbRoot() {
 
+    // Useful hooks
     const [shareId, setShareId] = useState("");
     const navigate = useNavigate();
 
+    // Evaluate entered shareId
     function handleChange(e) {
         setShareId(e.target.value);
     }
 
+    // Redirect on submit
     function handleRedirect() {
         navigate(`/share/${encodeURIComponent(shareId)}`);
     }
 
+    // Load files placeholder to quiet warnings
     function loadFilesPlaceHolder() {}
-
 
     return (
         <div>
